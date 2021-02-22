1079b35fc7625488c8e9091050cf43204826ba67
Dinu Barbu <dinu@MacBook-Pro.local>
2021-02-22 13:42:26 +0200

    scaffold logic

--- src/app/words.service.ts
+++ src/app/words.service.ts
@@ -36,4 +36,14 @@ export class WordsService {
     return of(this._words);
     // "of" produce un observable care emite o singura valoare, pe cea din parametru. se emite instantaneu, in momentul in care te abonezi la el
   }
+
+  /**
+   * approach 1 - return an array that repeats the elements from this._words - as many times as necessary to have a length > @param howMany
+   */
+  getManyWords1(howMany: number) {}
+
+  /**
+   * approach 2 - use another data source - a kind of lipsum package from npm. Generate exactly @param howMany random words with it
+   */
+  getManyWords2(howMany: number) {}
 }
