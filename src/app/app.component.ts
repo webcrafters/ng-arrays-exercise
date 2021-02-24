d6498125932a1a0236230b930703c86eb5481aab
Dinu Barbu <dinu@MacBook-Pro.local>
2021-02-24 08:38:44 +0200

    simplify in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,7 @@ export class AppComponent {
   words$ = this.wordsSvc.getWords();
   constructor(private wordsSvc: WordsService) {}
 
-  processWords(words: string[]): string[][] {
+  rowsFromWords(words: string[]): string[][] {
     return words.map((word) => word.split(''));
-  }
-
-  getTopFor(i: number, totalWords: number): string {
-    // solution based on setting the actual top value for the word-div
-
-    const divHeight = 18; // only works if we know the div height for any row to be 18px
-    const availableHeight = window.innerHeight;
-    const slotHeight = availableHeight / totalWords; // n words => n slots of this height
-    const adjustmentAtIndex = (slotHeight - divHeight) * (i / totalWords); // compensates for difference between divHeight and slotHeight
-    return `${slotHeight * i + adjustmentAtIndex}px`;
   }
 }
