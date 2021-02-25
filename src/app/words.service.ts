5cf308e224b810712903c4405b00189552935a8d
Dinu Barbu <dinu@MacBook-Pro.local>
2021-02-25 14:16:20 +0200

    solution complete

--- src/app/words.service.ts
+++ src/app/words.service.ts
@@ -6,73 +6,11 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root',
 })
 export class WordsService {
-  private _words: string[] = [
-    'angular',
-    'development',
-    'rendering',
-    'frontend',
-    'application',
-    'expression',
-    'closure',
-    'observable',
-    'array',
-    'code',
-    'subscription',
-    'return',
-    'angular',
-    'development',
-    'rendering',
-    'frontend',
-    'application',
-    'expression',
-    'closure',
-    'observable',
-    'array',
-    'code',
-    'return',
-  ];
-
   constructor(private lipsum: LoremIpsum) {}
 
-  getWords(): Observable<string[]> {
-    return of(this._words);
-    // "of" produce un observable care emite o singura valoare, pe cea din parametru. se emite instantaneu, in momentul in care te abonezi la el
-  }
-
   /**
-   * approach 1 - return an array that repeats the elements from this._words - as many times as necessary to have a length > @param howMany
-   */
-  getManyWords(howMany: number): Observable<string[]> {
-    const n = this._words.length;
-    const times = Math.ceil(howMany / n);
-
-    // sol 1
-    // imperative push
-    // const result = [];
-    // for (let i = 0; i < times; i++) {
-    //   result.push(...this._words);
-    // }
-
-    // sol 2
-    // imperative concat
-    // let result: string[] = [];
-    // for (let i = 0; i < times; i++) {
-    //   result = result.concat(this._words);
-    // }
-
-    // sol 3
-    // decalarative concat
-    // generate an array of copies of this_words
-    // flatten that array with concat and spread operator
-
-    const copies = new Array(times).fill(this._words);
-    const result = [].concat(...copies);
-
-    return of(result);
-  }
-
-  /**
-   * approach 2 - use another data source - a kind of lipsum package from npm. Generate exactly @param howMany random words with it
+   * use lipsum package to generate exactly @param howMany random words,
+   * @returns an observable that emits exactly once and then completes
    */
   getManyWordsLipsum(howMany: number): Observable<string[]> {
     return of(this.lipsum.generateWords(howMany).split(' '));
